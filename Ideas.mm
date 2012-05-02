e3f124e0b2601e96426af1c59d5875080629c8f4
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-03 07:17:07 +0900

    Initial

--- Ideas.mm
+++ Ideas.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1335969153109" ID="ID_1664202269" MODIFIED="1335969162937" TEXT="Ideas">
+<node CREATED="1335969153109" ID="ID_1664202269" MODIFIED="1335991941281" TEXT="Ideas">
 <node CREATED="1335969227515" ID="ID_990222195" MODIFIED="1335969250828" POSITION="right" TEXT="30&#x5206;&#x4ee5;&#x4e0a;&#x3044;&#x305f;&#x5834;&#x6240;&#x306f;&#x3001;GPS&#x3092;&#x8a18;&#x9332;&#x3059;&#x308b;"/>
 <node CREATED="1335969603750" ID="ID_590661780" MODIFIED="1335969614468" POSITION="right" TEXT="&#x82f1;&#x8a9e;&#x5b66;&#x7fd2;&#x3092;&#x52a9;&#x3051;&#x308b;&#x30a2;&#x30d7;&#x30ea;"/>
 <node CREATED="1335969623343" ID="ID_543861704" MODIFIED="1335969635656" POSITION="right" TEXT="&#x97f3;&#x697d;&#x30d5;&#x30a1;&#x30a4;&#x30eb;&#x3092;&#x7de8;&#x96c6;"/>
